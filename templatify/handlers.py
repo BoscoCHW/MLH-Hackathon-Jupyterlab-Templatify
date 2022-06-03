c93d6c59896a412044a8c2230ec1b1d0ac282e55
Bosco_Acer <bosco.chw@gmail.com>
2022-06-03 15:09:47 -0700

    implemented a post route to create notebook

--- templatify/handlers.py
+++ templatify/handlers.py
@@ -1,31 +1,42 @@
 import json
+from pathlib import Path
 
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
-import os
 import nbformat as nbf
 import tornado
+
+HERE = Path(__file__).parent.resolve()
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
     @tornado.web.authenticated
-    def get(self):
-        # req_body = self.get_json_body()
+    def post(self):
+        req_body = self.get_json_body()
+
+        with (HERE / "code_blocks.json").open() as f:
+            code_blocks = json.load(f)
         
+        cells = []
+        for category, category_config in req_body.items():
+            if category == "filename"
+            for key, val in category_config.items():
+                if val == True:
+                    cells.extend(code_blocks[category][key])
+            
         nb = nbf.v4.new_notebook()
-        text = """
-        # My first automatic Jupyter Notebook
-        This is an auto-generated notebook.
-        """
 
-        code = """
-        %pylab inline
-        hist(normal(size=2000), bins=50);
-        """
+        nb['cells'] = []
+        for cell in cells:
+            if (cell["type"] == "md"):
+                text = "\n".join(cell["text"])
+                nb["cells"].append(nbf.v4.new_markdown_cell(text))
+            elif (cell["type"] == "code"):
+                code = "\n".join(cell["text"])
+                nb["cells"].append(nbf.v4.new_code_cell(code))
 
-        nb['cells'] = [ nbf.v4.new_markdown_cell(text), nbf.v4.new_code_cell(code) ]
         nbf.write(nb, "test.ipynb")
 
         self.finish(json.dumps({

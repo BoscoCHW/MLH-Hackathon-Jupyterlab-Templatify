1c14cbcccffc7d5d2da818f1f32b62e0f950c3f2
Bosco_Acer <bosco.chw@gmail.com>
2022-06-08 10:12:58 -0700

    testing push 2

--- templatify/handlers.py
+++ templatify/handlers.py
@@ -21,7 +21,10 @@ class RouteHandler(APIHandler):
         
         cells = []
         for category, category_config in req_body.items():
-            # if category == "filename"
+            if category == "filePath":
+                path = category_config
+                path
+
             for key, val in category_config.items():
                 if val == True:
                     cells.extend(code_blocks[category][key])
